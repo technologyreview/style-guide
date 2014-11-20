30c05f4d800270ece18cedd2cbf0df44cecaa89d
Kevin Leary <info@kevinleary.net>
2014-11-20 10:34:43 -0500

    updates to template system:
    - add 404 & 500 error pages
    - updates to design to match mockup

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var express = require('express'),
 	app = express(),
 	favicon = require('serve-favicon'),
 	Twig = require('twig'),
-	randomstring = require('randomstring');
+	randomstring = require('randomstring'),
+	_ = require('underscore'),
+	semiStatic = require('semi-static'),
+	fs = require('fs'),
+	marked = require('marked');
 
 // livereload
 app.use(require('connect-livereload')({
@@ -21,9 +25,6 @@ app.set('twig options', {
 // twig config
 Twig.cache = false;
 Twig.extendFilter("markdown", function (value) {
-
-	// covert markdown to HTML
-	var marked = require('marked');
 	return marked(value);
 });
 
@@ -32,7 +33,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 // data: navigation
-var globals = {
+app.locals = _.extend(app.locals, {
 	menu: [{
 		label: 'Elements',
 		uri: '/'
@@ -49,32 +50,76 @@ var globals = {
 	bodyClass: 'nav-closed',
 	uuid: randomstring.generate(12),
 	organization: "MIT Technology Review"
-};
-
-/**
- * Route: Index
- *
- * Initial root page routing handler
- */
-app.get('/', function (req, res) {
-	res.render('layout', {
-		view: 'index.twig',
-		globals: globals
-	});
 });
 
 /**
- * Route: Pages
+ * Route middleware
+ *
+ * Track the current route as a local variable
+ * in templates, this get's used for current nav
+ * states
+ */
+app.use('*', function (req, res, next) {
+	app.locals = _.extend(app.locals, {
+		activeRoute: req.route
+	});
+	next();
+});
+
+/**
+ * Dynamic URL Routing
  *
  * Wildcard handler for all site pages, where the
  * URL path corresponds to a view template.
  */
 app.get('/*', function (req, res) {
-	var viewTemplate = (req.route) ? req.url.substring(1) : 'index';
-	res.render('layout', {
-		view: viewTemplate + '.twig',
-		globals: globals
+
+	// what view should we use based on the current route?
+	var viewPath = app.get('views');
+	var viewTemplate = (req.url === '/') ? 'index' : req.url.substring(1);
+	var templatePath = viewPath + '/' + viewTemplate + '.twig';
+
+	// does the view template for this route exist? if not serve an error page
+	fs.exists(templatePath, function (exists) {
+		if (exists) {
+			res.render('layout-default', {
+				view: viewTemplate + '.twig'
+			});
+		} else {
+			res.render('layout-simple', {
+				view: 'error-404.twig'
+			});
+		}
 	});
+
+});
+
+/**
+ * Error pages
+ */
+
+// 500
+app.use(function (error, req, res, next) {
+	res.status(500);
+
+	// HTML
+	if (req.accepts('html')) {
+		res.render('layout-simple', {
+			view: 'error-500.twig'
+		});
+		return;
+	}
+
+	// JSON
+	if (req.accepts('json')) {
+		res.send({
+			error: 'Internal Server Error'
+		});
+		return;
+	}
+
+	// Plain-text
+	res.type('txt').send('Internal Server Error');
 });
 
 /**
